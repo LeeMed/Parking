6f5671b79767d75364ced4e7b10dd13621f36583
WARDI Mohamed_Amine <Mohamed_Amine.Wardi@bordeaux-inp.fr>
2022-11-17 12:12:03 +0100

    qdouchgfkjdwhf

--- Parking/consultation.sql
+++ Parking/consultation.sql
@@ -9,4 +9,12 @@ where NUMERO_IMMATRICULATION = 'LI 743 RP';
 select NUMERO_DE_PLACE from STATIONNEMENT
 where DATE_STATIONNEMENT_E not null and DATE_STATIONNEMENT_S is null and ID_PARKING=1;
 
+with CTE as 
+(
+    select 1 Number
+    union all
+    select Number + 1 from CTE where Number < 100
+)
+select * from CTE
+
 
