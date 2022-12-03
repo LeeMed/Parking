0c711e629c03fc4d8305a751cfabae92c1431ec8
WARDI Mohamed_Amine <Mohamed_Amine.Wardi@bordeaux-inp.fr>
2022-12-03 21:06:21 +0100

    change home page

--- Parking/index.php
+++ Parking/index.php
@@ -1,6 +1,14 @@
 <title> The home page</title>
 <?php echo file_get_contents("html/header.html"); ?>
 <h1>
-This is the home page.
+Projet SGBD : Gestion des parkings
 </h1>
+
+L'objectif du projet est de mettre en ÷uvre, sur un cas pratique, les notions et les méthodes vues dans le module SGBD.
+
+
+<h2>Schéma conceptuel</h2>
+
+<img src = "./img/schema_conceptuel.png" style="width:800px;height:600px;">
+
 <?php echo file_get_contents("html/footer.html"); ?>
